9a573df60860ff472fad25ae4d2b09d4205f6715
szeidman <sam.zeidman@gmail.com>
2018-02-07 15:41:47 -0500

    Parse json from Citibike site to get individual station data

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
+const bodyParser = require('body-parser');
+const request = require('request');
 var express = require('express');
 var router = express.Router();
+
+router.use(bodyParser.urlencoded({ extended: true }));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,6 +11,17 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function (req, res) {
+  let station = req.body.station;
+  let url = 'https://feeds.citibikenyc.com/stations/stations.json';
+  request(url, function (err, response, body) {
+    if (err) {
+      res.render('index', {station: null, error: 'Error!'})
+    } else {
+      let stats = JSON.parse(body)
+      let stationlist = stats.stationBeanList
+      let selectedStation = stationlist.find(e => e.id == station);
+    }
+  })
   res.render('index');
 })
 
